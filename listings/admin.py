f30caea205e643edb10a8eaec9c944069a096c5f
Derek <derekluu90@gmail.com>
2019-11-20 21:41:14 -0500

    tweaked list and realtor models' admin

--- listings/admin.py
+++ listings/admin.py
@@ -3,5 +3,19 @@ from django.contrib import admin
 # in the same directory, import Listing. which is the name of the class in models
 from .models import Listing
 
+# extemds admin.ModelAdmin
+class ListingAdmin(admin.ModelAdmin):
+    # creates additional columns in admin area of listings
+    list_display = ('id', 'title', 'is_published', 'price', 'list_date', 'realtor')
+    # makes the arguments clickable as links
+    list_display_links = ('id', 'title')
+    # create filter - common is required as python expects a list / tuple. this is true for all the items here
+    list_filter = ('realtor',)
+    # creates checkmark, allowing to toggle listing between published and unpublished in admin panel
+    list_editable = ('is_published',)
+    # Searchable by the fields passed in
+    search_fields = ('title', 'description', 'address',  'city', 'state', 'zipcode', 'price')
+    list_per_page = 25
+
 # adds the model to django admin (localhost/admin)
-admin.site.register(Listing)
+admin.site.register(Listing, ListingAdmin)
--- realtors/admin.py
+++ realtors/admin.py
@@ -3,4 +3,10 @@ from django.contrib import admin
 # in the same directory, import realtorListing. which is the name of the class in models
 from .models import Realtor
 
-admin.site.register(Realtor)
+class RealtorAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'email', 'hire_date')
+    list_display_links = ('id', 'name')
+    search_fields = ('name',)
+    list_per_page = 25
+
+admin.site.register(Realtor, RealtorAdmin)
